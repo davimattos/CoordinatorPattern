149c83fd0655af57025d1c6e58680948aee1c319
davimattos <davimattosdev@gmail.com>
2023-08-24 15:28:57 -0300

    implements the coordinator

---
+++ CoordinatorPattern/MainCoodinator.swift
@@ -0,0 +1,29 @@
+//
+//  MainCoodinator.swift
+//  CoordinatorPattern
+//
+//  Created by Davi Mattos on 24/08/23.
+//
+
+import Foundation
+import UIKit
+
+class MainCoordinator: Coordinator {
+    var navigationController: UINavigationController?
+    
+    func eventOccurred(with type: Event) {
+        switch type {
+        case .buttonTapped:
+            var vc: UIViewController & Coordinating = SecondViewController()
+            vc.coordinator = self
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    func start() {
+        var vc: UIViewController & Coordinating = ViewController()
+        vc.coordinator = self
+        navigationController?.setViewControllers([vc], animated: false)
+    }
+}
+    
